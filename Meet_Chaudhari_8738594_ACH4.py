26cd3c67ea6f0fdab6dcfe7e647d9038f49e380d
Meet5161 <mchaudhari8594@conestogac.on.ca>
2021-11-08 01:31:42 -0500

    First Commit

---
+++ Meet_Chaudhari_8738594_ACH4.py
@@ -0,0 +1,30 @@
+print(" Select an operation to perform ")
+print ("1. ADD")
+print ("2. SUBTRACT")
+print ("3. MULTIPLICATION")
+print ("4. DIVISION \n")
+
+operation = input()
+
+if operation == "1":
+    num1 = input("Enter first number= ")
+    num2 = input("Enter second number= ")
+    print ("The answer is = " + str(int(num1) + int(num2)))
+
+elif operation == "2":
+    num1 = input("Enter first number=")
+    num2 = input("Enter second number=")
+    print ("The answer is = " + str(int(num1) - int(num2)))
+
+elif operation == "3":
+    num1 = input("Enter first number=")
+    num2 = input("Enter second number=")
+    print ("The answer is = " + str(int(num1) * int(num2)))
+
+elif operation == "4":
+    num1 = input("Enter first number=")
+    num2 = input("Enter second number=")
+    print ("The answer is = " + str(int(num1) / int(num2)))
+
+else:
+    print("Press 1 to 4")
